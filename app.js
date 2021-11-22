909e23a23e73dc14fa3e2703d03c336963dd5523
gaurang kathiriya <kathiriyagaurang44@gmail.com>
2021-11-22 11:10:33 +0530

    multer implemented

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const app = express()
 const cors = require('cors')
+const File = require("./src/models")
+const multer= require('multer')
+const DIR="public/"
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, DIR)
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+});
+
+var upload = multer({ storage: storage });
+
 
 
 app.use(cors())
 app.use(express.json())
 
+
 app.get("/",(req,res)=>{
     res.status(202).send(`<h1>welcome to image api home page </h1>`)
 })
 
-app.get('/images',(req,res)=>{
-    
+
+app.post('/upload',upload.single("img"),(req,res)=>{
+console.log(req.file);
 })
+
+
+app.get('/images',(req,res)=>{    
+})
+
+
+
 
 app.listen(3040,()=>console.log("server listening on port 3040"))
--- src/models.js
+++ src/models.js
@@ -1,5 +1,16 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    image
+createdAt:{
+      type:Date,
+      default:Date.now
+  },
+  name:{
+      type:String,
+      required:[true,"uploaded file must have a name"]
+  }
 })
+
+const File = mongoose.model("File",UserSchema)
+
+module.exports = File;
