7f8d4742dfdf7fe126087b8e7ea7296ad934d89a
gaurang kathiriya <kathiriyagaurang44@gmail.com>
2021-11-22 14:23:07 +0530

    api call of image implemented and store image using multer in public file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express()
 const cors = require('cors')
 const File = require("./src/models")
@@ -15,26 +16,22 @@ var storage = multer.diskStorage({
 
 var upload = multer({ storage: storage });
 
-
-
 app.use(cors())
 app.use(express.json())
 
+app.use(express.urlencoded({extended: true}))
 
 app.get("/",(req,res)=>{
     res.status(202).send(`<h1>welcome to image api home page </h1>`)
 })
 
 
-app.post('/upload',upload.single("img"),(req,res)=>{
+app.post('/upload',upload.single("file"),(req,res)=>{
 console.log(req.file);
 })
 
-
+    
 app.get('/images',(req,res)=>{    
 })
-
-
-
 
 app.listen(3040,()=>console.log("server listening on port 3040"))
